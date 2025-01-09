8789b576fa841304683298c92cb12419b9651159
Betül Şahin <54284545+betulsahinn@users.noreply.github.com>
2025-01-10 00:20:39 +0300

    Add files via upload
    sql

---
+++ contacts_202501100019.sql
@@ -0,0 +1,16 @@
+INSERT INTO public.contacts (full_name,email,phone,message,created_at,updated_at,status,is_archived,to_email) VALUES
+	 ('bet bet','201805002@stu.adu.edu.tr','(543) 454 67 67','aaa','2025-01-06 10:10:28.943+03','2025-01-06 10:10:28.943+03','new'::public.enum_contacts_status,false,'true'),
+	 ('bet bet','201805002@stu.adu.edu.tr','(543) 454 67 67','aaa','2025-01-06 10:11:41.37+03','2025-01-06 10:11:41.37+03','new'::public.enum_contacts_status,false,'true'),
+	 ('bet bet','201805002@stu.adu.edu.tr','(543) 454 67 67','aaa','2025-01-06 10:12:57.34+03','2025-01-06 10:12:57.34+03','new'::public.enum_contacts_status,false,'true'),
+	 ('bet bet','201805002@stu.adu.edu.tr','(543) 454 67 67','aaa','2025-01-06 10:14:51.886+03','2025-01-06 10:14:51.886+03','new'::public.enum_contacts_status,false,'true'),
+	 ('bet bet','201805002@stu.adu.edu.tr','(543) 454 67 67','aaa','2025-01-06 10:19:16.997+03','2025-01-06 10:19:16.997+03','new'::public.enum_contacts_status,false,'true'),
+	 ('bet bet','201805002@stu.adu.edu.tr','(543) 454 67 67','aaa','2025-01-06 10:19:35.879+03','2025-01-06 10:19:35.879+03','new'::public.enum_contacts_status,false,'true'),
+	 ('bet','201805002@stu.adu.edu.tr','(543) 454 67 67','a','2025-01-06 10:19:48.676+03','2025-01-06 10:19:48.676+03','new'::public.enum_contacts_status,false,'true'),
+	 ('betül şahin','201805002@stu.adu.edu.tr','(543) 454 67 67','deneme mesajı','2025-01-06 10:29:10.701+03','2025-01-06 10:29:10.701+03','new'::public.enum_contacts_status,false,'true'),
+	 ('bet','sahin9betul@gmail.com','(543) 454 67 67','deneme mesajı','2025-01-06 11:32:25.832+03','2025-01-06 11:32:25.832+03','new'::public.enum_contacts_status,false,'true'),
+	 ('bet','201805002@stu.adu.edu.tr','(543) 454 67 67','deneme','2025-01-06 11:33:37.134+03','2025-01-06 11:33:37.134+03','new'::public.enum_contacts_status,false,'true');
+INSERT INTO public.contacts (full_name,email,phone,message,created_at,updated_at,status,is_archived,to_email) VALUES
+	 ('betul deneme','bet@gmail.com','05679832578','deneme mesajı','2025-01-07 10:56:12.413+03','2025-01-07 10:56:12.413+03','new'::public.enum_contacts_status,false,'true'),
+	 ('deneme deneme','bet@gmail.com','05679832578','deneme deneme deneme','2025-01-07 11:05:55.564+03','2025-01-07 11:05:55.564+03','new'::public.enum_contacts_status,false,'iletisim@ronesis.com.tr'),
+	 ('deneme','deneme@gmail.com','5789873192','deneme','2025-01-07 11:15:25.872+03','2025-01-07 11:15:25.872+03','new'::public.enum_contacts_status,false,'iletisim@ronesis.com.tr'),
+	 ('bet','201805002@stu.adu.edu.tr','(543) 454 67 67','hhh','2025-01-08 11:57:22.286+03','2025-01-08 11:57:22.286+03','new'::public.enum_contacts_status,false,'iletisim@ronesis.com.tr');
